3679baf38acfa2a642f6abd862c76eaf5135a776
John Wible <john.wible@gmail.com>
2014-09-12 15:49:26 -0400

    Update Problem005.rb
    Added the Euler text

--- Problem005.rb
+++ Problem005.rb
@@ -15,3 +15,7 @@ while answer == 0
   x+=20                             #to limit the loops, if you count by 20's it wo
 end
 puts answer
+
+
+#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
